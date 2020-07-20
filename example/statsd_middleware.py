8d0d693bbf85d81d62eec9cbf219ba6a8349a52b
So1n <qaz6803609@163.com>
2020-07-20 11:59:43 +0800

    Add, add statsd middleware

---
+++ example/statsd_middleware.py
@@ -0,0 +1,59 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from fastapi_tools.statsd_middleware import StatsdClient, StatsdMiddleware
+from fastapi_tools.route_trie import RouteTrie
+
+
+app = FastAPI()
+client = StatsdClient()
+route_trie = RouteTrie()
+
+app.add_middleware(
+    StatsdMiddleware,
+    client=client,
+    route_trie=route_trie,
+    url_replace_handle=lambda url: url.replace('/', '_')
+)
+app.on_event("shutdown")(client.close)
+
+
+@app.on_event("startup")
+async def startup_event():
+    await client.connect()
+
+
+@app.get("/")
+async def root():
+    return {"Hello": "World"}
+
+
+@app.get("/api/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    """
+    copy from:https://fastapi.tiangolo.com/tutorial/query-params/#multiple-path-and-query-parameters
+    """
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+@app.get("/api/users/login")
+async def user_login():
+    return 'ok'
+
+
+# Note: The insert_by_app must be called after the all route added
+route_trie.insert_by_app(app, {"/metrics"})
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app)
---
+++ fastapi_tools/statsd_middleware.py
@@ -0,0 +1,65 @@
+import time
+from typing import Callable, Optional
+
+from aio_statsd import StatsdClient
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.base import RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.types import ASGIApp
+
+from fastapi_tools.route_trie import RouteTrie
+
+
+class StatsdMiddleware(BaseHTTPMiddleware):
+    def __init__(
+            self,
+            app: ASGIApp,
+            *,
+            client: StatsdClient,
+            app_name: str = 'fastapi_tools',
+            prefix: str = 'fastapi_tools',
+            route_trie: Optional['RouteTrie'] = None,
+            url_replace_handle: Optional[Callable] = None,
+    ) -> None:
+        super().__init__(app)
+        self._client: StatsdClient = client
+        self._route_trie: Optional[RouteTrie] = route_trie
+        self._metric = ''
+        self._url_replace_handle = url_replace_handle
+        if prefix:
+            self._metric += prefix + '.'
+        if app_name:
+            self._metric += app_name + '.'
+
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        method: str = request.method
+        url_path: str = request.url.path
+
+        if self._route_trie:
+            route = self._route_trie.search(url_path, request.scope)
+            if not route:
+                return await call_next(request)
+            else:
+                url_path = route.path
+        if self._url_replace_handle:
+            url_path = self._url_replace_handle(url_path)
+        metric: str = f'{self._metric}{method}.{url_path}.'
+        self._client.gauge(metric + 'request_in_progress', 1)
+        self._client.gauge(metric + 'request_count', 1)
+
+        status_code = 500
+        start_time = time.time()
+        request_result = 'fail'
+        try:
+            response = await call_next(request)
+            status_code = response.status_code
+            request_result = 'success'
+            return response
+        except Exception as e:
+            self._client.gauge(metric + f'exception.{type(e).__name__}', 1)
+            raise e
+        finally:
+            self._client.timer(metric + f'{request_result}.request_time', time.time() - start_time)
+            self._client.gauge(metric + f'{status_code}.response_count', 1)
+            self._client.gauge(metric + 'request_in_progress', -1)
