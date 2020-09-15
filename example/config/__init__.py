e5b687fcc39204742cd6c6dd4bf101b29db3ab4b
So1n <qaz6803609@163.com>
2020-09-16 00:05:51 +0800

    Modify, fix, config remove old code

--- example/config/__init__.py
+++ example/config/__init__.py
@@ -13,23 +13,26 @@ run output:
     {'name': 'ES_HOST', 'value': ['127.0.0.1:9200', '127.0.0.2:9200'], 'type': <class 'list'>}
 ]
 """
-from typing import Optional
+from typing import List, Optional
 from fastapi_tools.config import Config
+
+from pydantic.fields import Json
 
 
 class MyConfig(Config):
     DEBUG: bool
-    HOST: Optional[str]
-    PORT: Optional[int]
+    HOST: str
+    PORT: int
 
-    REDIS_ADDRESS: Optional[str]
+    REDIS_ADDRESS: str
     REDIS_PASS: Optional[str] = None
 
-    MYSQL_DB_HOST: Optional[str]
-    MYSQL_DB_NAME: Optional[str]
-    MYSQL_DB_PASS: Optional[str]
-    MYSQL_DB_USER: Optional[str]
-    ES_HOST: Optional[list]
+    MYSQL_DB_HOST: str
+    MYSQL_DB_NAME: str
+    MYSQL_DB_PASS: str
+    MYSQL_DB_USER: str
+    ES_HOST: Json[List]
+    TEST_LIST_INT: Json[List]
 
 
 config = MyConfig('./example_config.conf')
--- fastapi_tools/config.py
+++ fastapi_tools/config.py
@@ -1,8 +1,7 @@
-import ast
 import os
 import typing
 from collections.abc import MutableMapping
-from typing import Any, Dict, NoReturn, Union, Type
+from typing import Any, Dict, NoReturn, Type
 
 from pydantic import (
     BaseModel,
@@ -58,69 +57,20 @@ class Config:
         self, env_file: str = None,
         _environ: typing.Mapping[str, str] = environ
     ) -> None:
-        self._init_default_value()
         self._read_environ(_environ)
         if env_file is not None and os.path.isfile(env_file):
             self._read_file(env_file)
         pydantic_obj = self._init_pydantic_obj()
         self.__dict__.update(pydantic_obj(**self.__dict__).dict())
-        # self._check_not_set_value()
 
     def _init_pydantic_obj(self) -> Type[BaseModel]:
         annotation_dict: Dict[str, Type[Any, ...]] = {}
         for key in self.__annotations__:
             if hasattr(self, key):
+                self.__dict__[key] = getattr(self, key)
                 annotation_dict[key] = (self.__annotations__[key], ...)
         dynamic_model: Type[BaseModel] = create_model('DynamicFoobarModel', **annotation_dict)
         return dynamic_model
-
-    def _init_default_value(self):
-        for key in self.__annotations__:
-            if hasattr(self, key):
-                self.__dict__[key] = getattr(self, key)
-
-    def _check_not_set_value(self):
-        for key in self.__annotations__:
-            if not hasattr(self, key):
-                raise ValueError(f'{self.__class__.__name__}.{key} not set value')
-
-    def __setattr__(self, key: str, value: Any) -> NoReturn:
-        if key not in self.__annotations__:
-            raise AttributeError(
-                "{} not found attr:{}".format(self.__class__.__name__, key)
-            )
-        # Now support typing.Optional, typing.Union and python base type
-        key_type = self.__annotations__[key]
-        if hasattr(key_type, '__origin__') and key_type.__origin__ is Union:
-            # get typing.type from Union
-            key_type = key_type.__args__
-
-        if not isinstance(value, key_type):
-            try:
-                if isinstance(key_type, tuple):
-                    for i in key_type:
-                        try:
-                            value = self._python_type_conversion(i, value)
-                            break
-                        except TypeError:
-                            value = None
-                    else:
-                        raise TypeError
-                else:
-                    value = self._python_type_conversion(key_type, value)
-            except Exception:
-                raise TypeError(f"The type of {key} should be {key_type}")
-        self.__dict__[key] = value
-
-    @staticmethod
-    def _python_type_conversion(key_type, value: str):
-        value = ast.literal_eval(value)
-        if type(value) == key_type:
-            return value
-        try:
-            return key_type(value)
-        except Exception:
-            raise TypeError(f"Value type:{type(value)} is not {key_type}")
 
     def _read_environ(self, _environ: typing.Mapping[str, str]) -> NoReturn:
         for key in self.__annotations__.keys():
