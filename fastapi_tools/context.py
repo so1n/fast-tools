9526783af145a60b43331230aeab39cc14bf14c6
So1n <qaz6803609@163.com>
2020-10-18 00:05:09 +0800

    Modify, modify context type

--- fastapi_tools/context.py
+++ fastapi_tools/context.py
@@ -11,63 +11,63 @@ from starlette.responses import Response
 
 
 _CAN_JSON_TYPE_SET: Set[type] = {bool, dict, float, int, list, str, tuple, type(None)}
-_FASTAPI_TOOLS_CONTEXT: ContextVar[Dict[str, Any]] = ContextVar("fastapi_tools_context", default={})
 _NAMESPACE: str = 'fastapi_tools'
+_CONTEXT_KEY_SET: Set[str] = set()
+_CONTEXT_DICT_TYPE: type = Dict[str, Any]
+_FASTAPI_TOOLS_CONTEXT: ContextVar[Dict[str, Any]] = ContextVar(f"{_NAMESPACE}_context", default={})
 _MISS_OBJECT: object = object()
-_REQUEST_KEY: str = _NAMESPACE + ':request'
+_REQUEST_KEY: str = f'{_NAMESPACE}_request'
 
 
 class BaseContextHelper(object):
-    _set: set = set()
 
     def __init__(self, key: str):
         self._key: key = key
         cls: 'Type[BaseContextHelper]' = self.__class__
         key: str = f'{cls.__name__}:{key}'
-        if key in cls._set:
+        if key in _CONTEXT_KEY_SET:
             # key must be globally unique
             raise RuntimeError(f'key:{key} already exists')
-        cls._set.add(key)
+        _CONTEXT_KEY_SET.add(key)
 
-    def _get_context(self):
-        ctx_dict: dict = _FASTAPI_TOOLS_CONTEXT.get()
+    def _get_context(self) -> Any:
+        ctx_dict: _CONTEXT_DICT_TYPE = _FASTAPI_TOOLS_CONTEXT.get()
         return ctx_dict.get(self._key, _MISS_OBJECT)
 
-    def _set_context(self, value):
-        ctx_dict: dict = _FASTAPI_TOOLS_CONTEXT.get()
+    def _set_context(self, value: Any):
+        ctx_dict: _CONTEXT_DICT_TYPE = _FASTAPI_TOOLS_CONTEXT.get()
         ctx_dict[self._key] = value
 
     def __set__(self, instance: 'ContextBaseModel', value: Any):
         self._set_context(value)
 
-    def __get__(self, instance: 'ContextBaseModel', owner: 'Type[ContextBaseModel]'):
+    def __get__(self, instance: 'ContextBaseModel', owner: 'Type[ContextBaseModel]') -> Any:
         return self._get_context()
 
 
 class HeaderHelper(BaseContextHelper):
     def __init__(self, key: str, default_func: Optional[Callable] = None):
         self._default_func: Optional[Callable] = default_func
-
         super().__init__(key)
 
     def __set__(self, instance: 'ContextBaseModel', value: Any):
         raise NotImplementedError(f'{self.__class__.__name__} not support __set__')
 
-    def __get__(self, instance: 'ContextBaseModel', owner: 'Type[ContextBaseModel]'):
-        value = self._get_context()
+    def __get__(self, instance: 'ContextBaseModel', owner: 'Type[ContextBaseModel]') -> Any:
+        value: Any = self._get_context()
         if value is not _MISS_OBJECT:
             return value
 
-        ctx_dict: dict = _FASTAPI_TOOLS_CONTEXT.get()
+        ctx_dict: _CONTEXT_DICT_TYPE = _FASTAPI_TOOLS_CONTEXT.get()
         request: Request = ctx_dict[_NAMESPACE + ':request']
         headers: Headers = request.headers
         if self._key != self._key.lower():
-            value: Any = headers.get(self._key) or headers.get(self._key.lower())
+            value = headers.get(self._key) or headers.get(self._key.lower())
         else:
-            value: Any = headers.get(self._key)
+            value = headers.get(self._key)
 
         if not value and self._default_func is not None:
-            value: Any = self._default_func(request)
+            value = self._default_func(request)
         self._set_context(value)
         return value
 
@@ -79,7 +79,7 @@ class CustomHelper(BaseContextHelper):
 class ContextBaseModel(object):
     @classmethod
     def to_dict(cls, is_safe_return: bool = False) -> Dict[str, Any]:
-        _dict: dict = {}
+        _dict: _CONTEXT_DICT_TYPE = {}
         for key, value in get_type_hints(cls).items():
             value = getattr(cls, key)
             if is_safe_return and type(value) not in _CAN_JSON_TYPE_SET:
@@ -100,10 +100,8 @@ class ContextMiddleware(BaseHTTPMiddleware):
         super().__init__(*args, **kwargs)
         self.context_model: ContextBaseModel = context_model
 
-    async def dispatch(
-            self, request: Request, call_next: RequestResponseEndpoint
-    ) -> Response:
-        context_dict: dict = {_REQUEST_KEY: request}
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        context_dict: _CONTEXT_DICT_TYPE = {_REQUEST_KEY: request}
         token: Token = _FASTAPI_TOOLS_CONTEXT.set(context_dict)
         try:
             await self.context_model.before_request(request)
