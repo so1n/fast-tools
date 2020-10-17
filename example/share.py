54d91db72ef341f97389a3457ec1c70a845d9688
So1n <qaz6803609@163.com>
2020-10-18 00:51:25 +0800

    Add, module share

---
+++ example/share.py
@@ -0,0 +1,106 @@
+import asyncio
+import time
+from typing import Coroutine, List
+from fastapi_tools.share import Share
+
+
+async def delay_print(duration: int) -> int:
+    sleep_time: int = duration
+    if sleep_time > 3:
+        sleep_time = 3
+    await asyncio.sleep(sleep_time)
+    return duration
+
+
+async def run_do(share: 'Share'):
+    task_list: 'List[Coroutine]' = [
+        share.do('test_do', delay_print, args=[i])
+        for i in [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ]
+    print('run do start', time.time())
+    done, _ = await asyncio.wait(task_list)
+    print('run do end', time.time())
+    print(
+        'run do result',
+        [future.result() for future in done]
+    )
+
+
+async def cancel_in_aio(share: 'Share'):
+    await asyncio.sleep(0.1)
+    share.cancel()
+
+
+async def run_cancel(share: 'Share'):
+    task_list: 'List[Coroutine]' = [
+        share.do('test_cancel', delay_print, args=[i])
+        for i in [11, 12, 13, 14, 15, 16, 17, 18, 19]
+    ]
+    task_list.append(cancel_in_aio(share))
+    print('run cancel start', time.time())
+    try:
+        done, _ = await asyncio.wait(task_list)
+        print(
+            'run cancel result',
+            [future.result() for future in done]
+        )
+    except asyncio.CancelledError as e:
+        print('run cancel error: asyncio.CancelledError')
+    print('run cancel end', time.time())
+
+
+async def run_wapper_do(share: 'Share'):
+
+    @share.wrapper_do()
+    async def test_wapper_do(num):
+        print(f'call wapper :{num}')
+        return await delay_print(num)
+
+    task_list: 'List[Coroutine]' = [
+        test_wapper_do(i)
+        for i in [21, 22, 23, 24, 25, 26, 27, 28, 29]
+    ]
+    print('run wapper do start', time.time())
+    done, _ = await asyncio.wait(task_list)
+    print('run wapper do end', time.time())
+    print(
+        'run wapper do result',
+        [future.result() for future in done]
+    )
+
+
+async def run_class_wapper_do(share: 'Share'):
+    class TestClassWapperDo(object):
+
+        @share.wrapper_do()
+        async def delay_print(self, num: int):
+            print(f'call class wapper :{num}')
+            return await delay_print(num)
+
+    test_class_wapper_do: 'TestClassWapperDo' = TestClassWapperDo()
+
+    task_list: 'List[Coroutine]' = [
+        test_class_wapper_do.delay_print(i)
+        for i in [21, 22, 23, 24, 25, 26, 27, 28, 29]
+    ]
+    print('run class wapper do start', time.time())
+    done, _ = await asyncio.wait(task_list)
+    print('run class wapper do end', time.time())
+    print(
+        'run class wapper do result',
+        [future.result() for future in done]
+    )
+
+
+def main():
+    share: 'Share' = Share()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run_do(share))
+    loop.run_until_complete(run_cancel(share))
+    loop.run_until_complete(run_wapper_do(share))
+    loop.run_until_complete(run_class_wapper_do(share))
+
+
+if __name__ == '__main__':
+    main()
---
+++ fastapi_tools/share.py
@@ -0,0 +1,107 @@
+import asyncio
+from functools import wraps
+from typing import Any, Callable, Dict, TypeVar, Optional
+
+__all__ = ('Share', 'Token')
+_Tp = TypeVar('_Tp')
+
+
+class Token(object):
+    def __init__(self, key: str, share: 'Share'):
+        self._share: 'Share' = share
+        self._key: str = key
+        self._future: Optional[asyncio.Future] = None
+
+    def can_do(self) -> bool:
+        if not self._future:
+            self._future: 'asyncio.Future' = asyncio.Future()
+            return True
+        return False
+
+    def cancel(self) -> bool:
+        if self._future and not self._future.cancelled():
+            self._future.cancel()
+            return True
+        return False
+
+    async def await_done(self) -> Optional[Any]:
+        if not self._future:
+            raise RuntimeError(
+                f'You should use Token<{self._key}>.can_do() '
+                f'before Token<{self._key}>.await_done()'
+            )
+        if not self._future.done():
+            await self._future
+        return self._future.result()
+
+    def set_result(self, result: Any) -> bool:
+        if self._future and not self._future.done():
+            if isinstance(result, Exception):
+                self._future.set_exception(result)
+            else:
+                self._future.set_result(result)
+            asyncio.ensure_future(self._share.delay_del_token(self._key))
+            return True
+        return False
+
+
+class Share(object):
+    def __init__(self, delay_clean_time: int = 1):
+        self._future_dict: Dict[str, Token] = dict()
+        self._delay_clean_time = delay_clean_time
+
+    def get_token(self, key: str) -> Token:
+        if key not in self._future_dict:
+            token: 'Token' = Token(key, self)
+            self._future_dict[key] = token
+        return self._future_dict[key]
+
+    def cancel(self):
+        for key, token in self._future_dict.items():
+            token.cancel()
+
+    async def delay_del_token(self, key: str):
+        await asyncio.sleep(self._delay_clean_time)
+
+        if key in self._future_dict:
+            del self._future_dict[key]
+        else:
+            raise KeyError(f'not found token:{key}')
+
+    async def _token_handle(self, key: str, func: Callable, args: Optional[tuple], kwargs: Optional[dict]) -> Any:
+        args: list = args if args else []
+        kwargs: dict = kwargs if kwargs else {}
+        token: Token = self.get_token(key)
+        if token.can_do():
+            try:
+                result = await func(*args, **kwargs)
+                token.set_result(result)
+            except Exception as e:
+                token.set_result(e)
+                raise e
+        return await token.await_done()
+
+    async def do(
+        self,
+        key: str,
+        func: Callable,
+        args: Optional[list] = None,
+        kwargs: Optional[dict] = None
+    ) -> Any:
+        return await self._token_handle(key, func, args, kwargs)
+
+    def wrapper_do(self, key: Optional[str] = None):
+        def wrapper(func: Callable):
+            if key is None:
+                key_name: str = func.__name__ + str(id(func))
+            else:
+                key_name: str = key
+
+            @wraps(func)
+            async def wrapper_func(*args, **kwargs):
+                return await self._token_handle(key_name, func, args, kwargs)
+            return wrapper_func
+        return wrapper
+
+    def __str__(self):
+        return str(self._future_dict)
