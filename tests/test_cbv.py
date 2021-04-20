d6dc8d80f812ad5a731c4f25ae56f88a529c8f99
so1n <qaz6803609@163.com>
2021-04-21 00:26:13 +0800

    Add, test&fix, add cbv test and remove cbv_decorator can not use param

---
+++ tests/test_cbv.py
@@ -0,0 +1,34 @@
+import asyncio
+import aioredis  # type: ignore
+import time
+import pytest
+from requests import Response
+from starlette.testclient import TestClient
+
+from example.cbv import app
+from fast_tools.base.redis_helper import RedisHelper
+from .conftest import AnyStringWith  # type: ignore
+
+
+pytestmark = pytest.mark.asyncio
+
+
+class TestCbv:
+    def test_cbv(self) -> None:
+        with TestClient(app) as client:
+            response: Response = client.get("/?test_default_id=345")
+            assert response.status_code == 203
+            assert response.json() == {
+                "message": "hello, world",
+                "user_agent": "testclient",
+                "host": "testserver",
+                "id": 345
+            }
+            response = client.post("/")
+            assert response.status_code == 200
+            assert response.json() == {
+                "message": "hello, world",
+                "user_agent": "testclient",
+                "host": "testserver",
+                "id": 123
+            }
