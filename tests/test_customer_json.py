fba21121dab8134609fa99eebdaa86708881a513
so1n <qaz6803609@163.com>
2021-04-24 00:32:24 +0800

    Add, test, add json test

--- tests/test_customer_json.py
+++ tests/test_customer_json.py
@@ -5,11 +5,16 @@ from fast_tools import customer_json as json
 
 
 class TestCustomerJson:
-    def test_customer_json(self):
+    def test_customer_json(self) -> None:
         test_dict: dict = {
             "datetime": datetime.datetime.now(),
             "date": datetime.datetime.now().date(),
             "decimal": Decimal("1.01"),
             "time": datetime.time(),
         }
-        print(json.dumps(test_dict))
+        assert json.loads(json.dumps(test_dict)) == {
+            "datetime": test_dict["datetime"].strftime("%Y-%m-%d %H:%M:%S"),
+            "date": test_dict["date"].strftime("%Y-%m-%d"),
+            "decimal": float(test_dict["decimal"]),
+            "time": test_dict["time"].strftime("%H:%M:%S"),
+        }
