d99bb88b90199692daf04080104840c16ad647b2
so1n <qaz6803609@163.com>
2021-07-10 02:22:08 +0800

    Modify, Fix, fix lazy property

--- fast_tools/lazy_property.py
+++ fast_tools/lazy_property.py
@@ -3,28 +3,35 @@ from concurrent import futures
 from typing import Any, Callable, Optional, Type
 
 
-class _FakeClass:
+class _BoundClass(object):
     pass
 
 
+_bound_class: _BoundClass = _BoundClass()
+
+
 class LazyProperty:
+    """Cache field computing resources
+    >>> class Demo:
+    ...     @LazyProperty(is_class_func=True)
+    ...     def value(self, value):
+    ...         return value * value
+    """
     def __init__(self, is_class_func: bool = False):
         self._is_class_func: bool = is_class_func
-        self._class: Any = _FakeClass()
 
     def __call__(self, func: Callable) -> Callable:
+        key: str = f"{self.__class__.__name__}_{id(func)}_future"
         if not asyncio.iscoroutinefunction(func):
 
             def wrapper(*args: Any, **kwargs: Any) -> Any:
-                class_: Any = args[0] if self._is_class_func else self._class
-                future: Optional[futures.Future] = getattr(
-                    class_, f"{self.__class__.__name__}_{func.__name__}_future", None
-                )
+                class_: Any = args[0] if self._is_class_func else _bound_class
+                future: Optional[futures.Future] = getattr(class_, key, None)
                 if not future:
                     future = futures.Future()
                     result: Any = func(*args, **kwargs)
                     future.set_result(result)
-                    setattr(class_, f"{self.__class__.__name__}_{func.__name__}_future", future)
+                    setattr(class_, key, future)
                     return result
                 return future.result()
 
@@ -32,15 +39,13 @@ class LazyProperty:
         else:
 
             async def async_wrapper(*args: Any, **kwargs: Any) -> Any:
-                class_: Type = args[0] if self._is_class_func else self._class
-                future: Optional[asyncio.Future] = getattr(
-                    class_, f"{self.__class__.__name__}_{func.__name__}_future", None
-                )
+                class_: Any = args[0] if self._is_class_func else _bound_class
+                future: Optional[asyncio.Future] = getattr(class_, key, None)
                 if not future:
                     future = asyncio.Future()
                     result: Any = await func(*args, **kwargs)
                     future.set_result(result)
-                    setattr(class_, f"{self.__class__.__name__}_{func.__name__}_future", future)
+                    setattr(class_, key, future)
                     return result
                 return future.result()
 
